d7d964d11daf75cf12f96659836cbd33331a7577
hitorimakuro <marsek4@yandex.ru>
2024-12-07 00:03:28 +0300

    added 2 new emotions: fear and disgust

--- src/components/face/face.tsx
+++ src/components/face/face.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 import { Button, cn } from "@nextui-org/react";
 const Face = ({ className }: { className?: string }) => {
   function changeExpression(
-    expression: "sad" | "happy" | "angry" | "surprised",
+    expression: "sad" | "happy" | "angry" | "surprised"
   ) {
     const mouth = document.querySelector(".mouth") as HTMLElement;
     const brow_left = document.querySelector(".brow.left") as HTMLElement;
@@ -187,6 +187,103 @@ const Face = ({ className }: { className?: string }) => {
 
         break;
 
+      case "fear":
+        anime.remove(mouth);
+        anime.remove(brow_left);
+        anime.remove(brow_right);
+        anime.remove(brow);
+        anime.remove(eye_left);
+        anime.remove(eye_right);
+        anime.remove(eye);
+        mouth.style.transform = "translateX(-50%) scaleY(1.5) scaleX(0.7)";
+        brow_left.style.transform = "rotate(-25deg) translateY(-80%)";
+        brow_right.style.transform = "rotate(25deg) translateY(-80%)";
+        mouth.style.borderRadius = "50";
+        anime({
+          targets: ".eye",
+          scale: [1, 1.3], // Увеличение глаз
+          duration: 500,
+          easing: "easeInOutSine",
+        });
+        anime({
+          targets: ".eye",
+          translateY: [0, 1], // Легкое движение вверх и вниз
+          scale: 1.3, // Увеличение глаз
+          duration: 500,
+          // scale: 1,
+          easing: "easeInOutSine",
+          direction: "alternate",
+          loop: true,
+        });
+        anime({
+          targets: ".mouth",
+          translateY: [-2, 2], // Легкое движение вверх и вниз
+          duration: 500,
+          easing: "easeInOutSine",
+          direction: "alternate",
+          loop: true,
+        });
+        anime({
+          targets: ".brow",
+          translateY: [-300, 0], // Движение вверх и вниз
+          duration: 400,
+          easing: "easeInOutSine",
+          direction: "alternate",
+          loop: true,
+        });
+        break;
+
+      case "disgust":
+        anime.remove(mouth);
+        anime.remove(brow_left);
+        anime.remove(brow_right);
+        anime.remove(eye_left);
+        anime.remove(eye_right);
+        // Настройка элементов
+        mouth.style.transform = "translateX(-50%) rotate(-15deg)";
+        mouth.style.borderRadius = "0 50px 50px 0";
+        mouth.style.width = "40px";
+        mouth.style.height = "20px";
+        // brow_left.style.transform = 'rotate(20deg) translateY(-10px)';
+        // brow_right.style.transform = 'rotate(-20deg) translateY(5px)';
+        brow_left.style.transform = "rotate(-25deg)";
+        brow_right.style.transform = "rotate(25deg)";
+        // eye_left.style.width = '20px';
+        // eye_left.style.height = '15px';
+        // eye_right.style.width = '20px';
+        // eye_right.style.height = '15p
+        // Анимация рта
+        anime({
+          targets: ".mouth",
+          translateX: [-2, 2], // Легкое движение из стороны в сторону
+          duration: 600,
+          easing: "easeInOutSine",
+          direction: "alternate",
+          loop: true,
+        });
+
+        // Анимация бровей
+        anime({
+          targets: [".brow.left", ".brow.right"],
+          translateY: [-2, 2],
+          duration: 500,
+          easing: "easeInOutSine",
+          direction: "alternate",
+          loop: true,
+        });
+
+        // Анимация глаз
+        anime({
+          targets: [".eye.left", ".eye.right"],
+          scale: 1,
+          // scaleY: [1, 0.9], // Легкое моргание
+          duration: 400,
+          easing: "easeInOutSine",
+          direction: "alternate",
+          loop: true,
+        });
+        break;
+
       default:
         break;
     }
@@ -220,7 +317,12 @@ const Face = ({ className }: { className?: string }) => {
   //   });
   // }, []);
   return (
-    <div className={cn("flex flex-col gap-4 justify-center min-[400px]:flex-row items-center", className)}>
+    <div
+      className={cn(
+        "flex flex-col gap-4 justify-center min-[400px]:flex-row items-center",
+        className
+      )}
+    >
       <div className="face">
         <div className="brow left" />
         <div className="brow right" />
@@ -234,6 +336,8 @@ const Face = ({ className }: { className?: string }) => {
         <Button onClick={() => changeExpression("happy")}>Радость</Button>
         <Button onClick={() => changeExpression("angry")}>Гнев</Button>
         <Button onClick={() => changeExpression("surprised")}>Удивление</Button>
+        <Button onClick={() => changeExpression("fear")}>Страх</Button>
+        <Button onClick={() => changeExpression("disgust")}>Отвращение</Button>
       </div>
     </div>
   );
